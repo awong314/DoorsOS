5aff294ee9d0823c8ad73bac6bfffdd57465731b
Andrew Wong <wonga@athena.ecs.csus.edu>
2018-02-08 19:34:27 -0800

    IdleProc and UserPro

--- Phase1/proc.c
+++ Phase1/proc.c
@@ -10,16 +10,22 @@ void IdleProc(void) {
    int i;
 
    while(1) {
-      show on target PC: "0 "    // SystemProc has PID 0
-      repeat LOOP times: call asm("inb $0x80")  // to cause delay approx 1 second
+      cons_printf("0 ");    // SystemProc has PID 0
+      for(i=0; i<LOOP; i++) {
+         asm("inb $0x80");  // to cause delay approx 1 second
+      }
    }
 }
 
 void UserProc(void) {
    int i;
 
-   forever loop {
-      show on target PC own PID (run_pid) using cons_printf()
-      repeat LOOP times: call asm("inb $0x80")  // to cause delay approx 1 second
+   while(1) {
+      //show on target PC own PID (run_pid) using cons_printf()
+      cons_printf("%d", run_pid);
+      //repeat LOOP times: call asm("inb $0x80")  // to cause delay approx 1 second
+      for(i=0; i<LOOP; i++) {
+         asm("inb $0x80");
+      }
    }
 }

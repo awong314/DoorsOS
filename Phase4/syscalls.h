2dae44bc669d645a6769e6c25279123b2b019df8
Andrew Wong <andrewwong@csus.edu>
2018-02-26 15:33:09 -0800

    Phase 3 complete

---
+++ Phase4/syscalls.h
@@ -0,0 +1,13 @@
+#ifndef _SYSCALLS_H_
+#define _SYSCALLS_H_
+
+#include "kernel_constants.h"
+
+extern int sys_getpid(void);
+extern void sys_write(int, char*, int);
+extern void sys_sleep(int);
+extern void sys_semwait(int);
+extern void sys_sempost(int);
+
+
+#endif

e80f88f0adf8e972b0ecbc15c7bf75d867c6e1de
Andrew Wong <andrewwong@csus.edu>
2018-04-25 16:17:09 -0700

    Phase 8 Changes

---
+++ Phase9/kernel_types.h
@@ -0,0 +1,70 @@
+// kernel_types.h, 159
+
+#ifndef _KERNEL_TYPES_H_
+#define _KERNEL_TYPES_H_
+
+#include "kernel_constants.h"
+
+typedef void (*func_p_t)(); // void-return function pointer type
+
+// SLEEP state added in Phase2
+// WAITCHILD and ZOMBIE added in Phase 8
+typedef enum {AVAIL, READY, RUN, SLEEP, WAIT, WAITCHILD, ZOMBIE} state_t;   
+
+typedef struct {
+   //Top 4 regs Phase 6
+   unsigned int edi,
+               esi,
+               ebp,
+               esp,     
+               ebx,
+		         edx,
+		         ecx,
+		         eax,
+		         intr_num,
+		         eip,
+		         cs,
+		         efl;
+} trapframe_t;
+
+typedef struct {
+   //Phase 1
+   state_t state;             // state of process
+   int runtime;               // runtime of this run
+   int totaltime;             // total runtime
+   trapframe_t *trapframe_p;  // points to saved trapframe
+   //Phase 2
+   int wake_time;	            // wake time of proc in centi-secs = cur OS time + sleep time 
+   //Phase 6
+   int ppid;                  // Parent pid         
+   //Phase 9
+   int page; 
+} pcb_t;                     
+
+typedef struct {              // generic queue type
+   int q[Q_SIZE];             // integers are queued in q[] array
+   int size;                  // size is also where the tail is for new data
+   int indexOfFirst;          // index of the first value
+} pid_q_t;
+
+
+// Phase 3
+typedef struct {
+   int val;
+   pid_q_t wait_q;
+} semaphore_t;
+
+
+// Phase 4
+typedef struct {              // phase4 Dev Drv I
+   char dsp[BUFF_SIZE];       // buffer for term output
+   int port;                  // port data register
+   pid_q_t dsp_wait_q;        // PID await for term output 
+
+   // Phase 5
+   char kb[BUFF_SIZE];        // Keyboard buffer
+   pid_q_t kb_wait_q;         
+   int status;   
+} term_t;
+
+#endif // _KERNEL_TYPES_H_
---
+++ Phase9/proc_syscalls.c
@@ -0,0 +1,160 @@
+// syscalls.c
+// calls to kernel services
+
+#include "kernel_constants.h" // SYS_WRITE 4, SYS_GETPID 20, etc.
+#include "kernel_types.h"
+
+int sys_getpid(void) {
+   int pid;
+
+   asm("movl %1, %%eax;       // service #20 (SYS_GETPID)
+      int $128;	            // interrupt CPU with IDT entry 128
+      movl %%ebx, %0;"	      // after, copy ebx to variable 'pid'
+      : "=g" (pid)	         // output syntax
+      : "g" (SYS_GETPID)      // input syntax
+      : "eax", "ebx" 	      // used registers
+   );
+
+   return pid;
+}
+
+void sys_write(int fileno, char *str, int len) {
+   if(*str == '\0') return;
+
+   asm("movl %0, %%eax;       // service #4 (SYS_WRITE) via eax
+      movl %1, %%ebx;	      // send in fileno via ebx (e.g., STDOUT)
+      movl %2, %%ecx;         // send in str addr via ecx
+      movl %3, %%edx;         // send in str length via edx
+      int $128;" 	            // initiate service call, intr 128 (IDT entry 128)
+      : 
+      : "g" (SYS_WRITE), "g" (fileno), "g" ((int)str), "g" (len)
+      : "eax", "ebx", "ecx", "edx"
+   ); 
+} 
+
+//Phase 5
+void sys_read(int fileno, char *str, int len) {
+   if(len==0) return;
+
+   asm("movl %0, %%eax;       // service #3 (SYS_READ) via eax
+      movl %1, %%ebx;	      // send in fileno via ebx (e.g., STDOUT)
+      movl %2, %%ecx;         // send in str addr via ecx
+      movl %3, %%edx;         // send in str length via edx
+      int $128;" 	            // initiate service call, intr 128 (IDT entry 128)
+      : 
+      : "g" (SYS_READ), "g" (fileno), "g" ((int)str), "g" (len)
+      : "eax", "ebx", "ecx", "edx"
+   ); 
+} 
+
+void sys_sleep(int centi_sec) {	// 1 centi-second is 1/100 of a second
+   asm("movl %0, %%eax;       // Service #162 SLEEP
+      movl %1, %%ebx;	      // send in centi-seconds via ebx
+      int $128;"
+      :
+      : "g" (SYS_SLEEP), "g" (centi_sec)
+      : "eax", "ebx"
+   );
+}
+
+void sys_semwait(int sem_num) {
+  asm("movl %0, %%eax;        // Service #300 SEMWAIT
+      movl %1, %%ebx;	      // send in sem_num via ebx
+      int $128;"
+      :
+      : "g" (SYS_SEMWAIT), "g" (sem_num)
+      : "eax", "ebx"
+   );
+}
+
+void sys_sempost(int sem_num) {
+   asm("movl %0, %%eax;       // Service #301 SEMPOST
+      movl %1, %%ebx;	      // send in sem_num via ebx
+      int $128;"
+      :
+      : "g" (SYS_SEMPOST), "g" (sem_num)
+      : "eax", "ebx"
+   );
+} 
+
+//Phase 6
+int sys_fork(void) {
+   int pid;
+
+   asm("movl %1, %%eax;       // Service #2 Fork
+      int $128;
+      movl %%ebx, %0;"	      // after, copy ebx to variable 'pid'
+      : "=g" (pid)
+      : "g" (SYS_FORK)
+      : "eax", "ebx"
+   );
+
+   return pid;
+}
+
+//Phase 7
+int sys_getppid() {
+   int pid;
+
+   asm("movl %1, %%eax;       
+      int $128;	           
+      movl %%ebx, %0;"	      
+      : "=g" (pid)	         
+      : "g" (SYS_PPID)      
+      : "eax", "ebx" 	      
+   );
+   return pid;
+}
+
+void sys_signal(int signal, func_p_t p) {
+  asm("movl %0, %%eax;        // Service #48 Signal
+      movl %1, %%ebx;	      // send in signal via ebx 
+      movl %2, %%ecx;         // send in p addr via ecx
+      int $128;"
+      :
+      : "g" (SYS_SIGNAL), "g" (signal), "g" ((int)p)
+      : "eax", "ebx", "ecx"
+   ); 
+}
+
+// PHASE 8
+void sys_exit(int exit_code) {
+   asm("movl %0, %%eax;
+      movl %1, %%ebx;    // put exit_code to ebx
+      int $128;"         
+      :
+      :  "g" (SYS_EXIT), "g" (exit_code)
+      :  "eax", "ebx"
+   );
+}
+
+int sys_waitchild(int *exit_code_p) {
+   int child_pid;
+   asm(" movl %1, %%eax;
+      movl %2, %%ebx;
+      int $128;
+      movl %%ecx, %0"   // copy ecx to child_pid
+      :  "=g" (child_pid)
+      :  "g" (SYS_WAITCHILD), "g" ((int)exit_code_p)
+      :  "eax", "ebx", "ecx"
+   );
+   return child_pid;
+}
+
+// Phase 9
+void sys_exec(func_p p, int arg) {
+
+   asm("movl %0, %%eax;        
+      movl %1, %%ebx;	      // send in arg via ebx 
+      movl %2, %%ecx;         // send in p addr via ecx
+      int $128;"
+      :
+      : "g" (SYS_EXEC), "g" (arg), "g" ((int)p)
+      : "eax", "ebx", "ecx"
+   );
+}
+
+
+
+
+
